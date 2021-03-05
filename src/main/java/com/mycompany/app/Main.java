ec1b367b5118be143532144a60be6af22a70809c
Nono98 <noemie.plancherel@heig-vd.ch>
2021-03-05 17:34:58 +0100

    Ajout Main Picocli

---
+++ src/main/java/com/mycompany/app/Main.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(
+        name = "Main"
+)
+public class Main implements Runnable {
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello Run Main Picocli!");
+    }
+
+}
