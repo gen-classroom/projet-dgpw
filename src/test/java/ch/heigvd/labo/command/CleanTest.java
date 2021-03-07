e994bb9fa1eeb1bedc2c7f6f4abed13465316f94
Nono98 <noemie.plancherel@heig-vd.ch>
2021-03-07 15:05:04 +0100

    Ajouter la sous-commande Picocli clean

---
+++ src/test/java/ch/heigvd/labo/command/CleanTest.java
@@ -0,0 +1,16 @@
+package ch.heigvd.labo.command;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CleanTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
