a8c82c198f9ab2450bd1602f692674369b04ac93
CassandreWoj <cassandre.wojciechowski@heig-vd.ch>
2021-03-07 15:35:17 +0100

    Ajout commande build + test unitaire

---
+++ src/test/java/ch/heigvd/labo/command/BuildTest.java
@@ -0,0 +1,16 @@
+package ch.heigvd.labo.command;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BuildTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
