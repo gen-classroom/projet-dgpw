92b2bd30420f226289b56920930cd77d64af5157
Dosseggegw1 <gwen.dos@bluewin.ch>
2021-03-17 16:14:45 +0100

    Mise à jour du test unitaire

--- src/test/java/ch/heigvd/labo/MainTest.java
+++ src/test/java/ch/heigvd/labo/MainTest.java
@@ -1,10 +1,20 @@
 package ch.heigvd.labo;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
 
 public class MainTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(output));
+    }
+
     /**
      * Rigorous Test :-)
      */
@@ -12,5 +22,12 @@ public class MainTest {
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldDisplayVersion(){
+        String strSouhaite = "DGPW version";
+        new CommandLine(new Main()).execute("-v");
+        assertTrue(output.toString().contains(strSouhaite));
     }
 }
