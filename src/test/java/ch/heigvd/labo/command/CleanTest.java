3dc6f259465892e783702379361a333d9eab5981
Dosseggegw1 <gwen.dos@bluewin.ch>
2021-03-18 15:53:49 +0100

    Implémentation de clean en cours

--- src/test/java/ch/heigvd/labo/command/CleanTest.java
+++ src/test/java/ch/heigvd/labo/command/CleanTest.java
@@ -1,11 +1,14 @@
 package ch.heigvd.labo.command;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import ch.heigvd.labo.Main;
+import org.junit.jupiter.api.*;
 import picocli.CommandLine;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.apache.commons.io.FileUtils;
 
 public class CleanTest {
     /**
