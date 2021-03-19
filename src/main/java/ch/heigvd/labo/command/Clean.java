50b5524af53e7a275f077f39d1c120815dbe6849
Dosseggegw1 <gwen.dos@bluewin.ch>
2021-03-19 11:26:43 +0100

    Implementation units tests

--- src/main/java/ch/heigvd/labo/command/Clean.java
+++ src/main/java/ch/heigvd/labo/command/Clean.java
@@ -1,11 +1,18 @@
 package ch.heigvd.labo.command;
 
+import java.io.File;
 import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(name = "clean", description ="Clean .... ???")
+@Command(name = "clean", description ="Nettoie le répertoire build du site statique")
 public class Clean implements Callable<Integer> {
+    @CommandLine.Parameters(paramLabel = "FILE", description = "répertoire du site statique qu'on veut clean")
+    File dir;
+
     @Override public Integer call(){
+        
         System.out.printf("clean");
         return 1;
     }
--- src/test/java/ch/heigvd/labo/command/CleanTest.java
+++ src/test/java/ch/heigvd/labo/command/CleanTest.java
@@ -10,13 +10,51 @@ import java.io.IOException;
 import java.nio.file.Files;
 import org.apache.commons.io.FileUtils;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CleanTest {
+    static final String DIR_TO_REMOVE = "www/test_init";
+
     /**
-     * Rigorous Test :-)
+     * Create a test repertory
+     */
+    @BeforeAll
+    static void createRepertoryTest() throws Exception{
+        File dir = new File(DIR_TO_REMOVE + "/build");
+        try {
+            if (!dir.mkdirs()) {
+                throw new IOException("Impossible de créer le répertoire");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Delete an existant repertory
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @Order(1)
+    void shouldDeleteExistantRepertory()throws Exception{
+        File dir = new File(DIR_TO_REMOVE);
+        int exitCode = new CommandLine(new Clean()).execute(dir);
+        assertEquals(exitCode, 0);
+        assertThrows(IOException.class, () -> {
+            throw new IOException("Impossible de supprimer le répertoire");
+        });
     }
+
+    /**
+     * Try to delete an inexistant repertory
+     */
+    @Test
+    @Order(2)
+    void shouldNotDeleteInexistantRepertory() throws Exception {
+        File dir = new File(DIR_TO_REMOVE);
+        int exitCode = new CommandLine(new Clean()).execute(dir);
+        assertEquals(exitCode, 1);
+        assertThrows(IOException.class, () -> {
+            throw new IOException("Le répertoire est inexistant");
+        });
+    }
+
 }
