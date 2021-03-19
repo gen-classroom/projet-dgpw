29054e50c1519fbec122552f5ee52c0737b507ce
CassandreWoj <cassandre.wojciechowski@heig-vd.ch>
2021-03-19 13:05:41 +0100

    ajout de la création de fichier de conf

--- src/main/java/ch/heigvd/labo/command/Init.java
+++ src/main/java/ch/heigvd/labo/command/Init.java
@@ -1,6 +1,9 @@
 package ch.heigvd.labo.command;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -11,17 +14,44 @@ public class Init implements Callable<Integer> {
     // Répertoire racine de notre site statique
     static final String DIR_RACINE = "www/";
 
-    @Override public Integer call(){
+    @Override public Integer call() throws IOException {
         File dir = new File(DIR_RACINE + file.getPath());
 
         if (!dir.mkdirs()) {
-            System.out.println("Le création du répertoire a échoué ou le répertoire existe déjà");
+            System.out.println("La création du répertoire a échoué ou le répertoire existe déjà");
             return 1;
         } else {
             System.out.println("Le répertoire a été créé");
+
+            // Création du fichier de configuration
+            try {
+                File conf = new File(dir + "/config.yaml");
+                boolean created = conf.createNewFile();
+
+                if (created) {
+                    System.out.println("Le fichier de config a été créé : " + conf.getAbsolutePath());
+
+                    // Ajout du template dans le fichier de config créé
+                    FileWriter writing = new FileWriter(conf, true);
+                    PrintWriter printing = new PrintWriter(writing);
+                    printing.append("indexfile: #inserer nom de fichier index\n" +
+                            "title: #inserer titre \n" +
+                            "sourcedir:#inserer repertoire source\n" +
+                            "datadir:.\n" +
+                            "filesdir:#inserer repertoire fichiers\n");
+                    printing.close();
+
+                } else {
+                    System.out.println("Un problème a été rencontré et le fichier n'a pas été créé.");
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
             return 0;
         }
     }
+
     @Parameters(paramLabel = "FILE", description = "répertoire pour site statique")
     File file;
 }
