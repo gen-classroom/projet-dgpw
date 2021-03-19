0b2a5410f730d0317d88a7fc173b9854f07941c7
Dosseggegw1 <gwen.dos@bluewin.ch>
2021-03-19 12:12:30 +0100

    Clean ok

--- src/main/java/ch/heigvd/labo/command/Clean.java
+++ src/main/java/ch/heigvd/labo/command/Clean.java
@@ -1,19 +1,38 @@
 package ch.heigvd.labo.command;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
+import org.apache.commons.io.FileUtils;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "clean", description ="Nettoie le répertoire build du site statique")
 public class Clean implements Callable<Integer> {
-    @CommandLine.Parameters(paramLabel = "FILE", description = "répertoire du site statique qu'on veut clean")
+    @CommandLine.Parameters(paramLabel = "FILE", description = "Chemin du répertoire du site statique à nettoyer")
     File dir;
 
     @Override public Integer call(){
-        
-        System.out.printf("clean");
-        return 1;
+        if (!dir.exists()) {
+            System.out.format("Impossible d'accéder au répertoire %s. Celui-ci est inexistant\n",dir.getName());
+            return 1;
+        }
+
+        File dirBuild = new File(dir.getPath()+"/build");
+
+        if(!dirBuild.exists()){
+            System.out.format("Impossible de supprimer %s. Celui-ci est inexistant\n",dirBuild.getName());
+            return 2;
+        }
+
+        try {
+            FileUtils.deleteDirectory(dirBuild);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Le répertoire a été supprimé.");
+        return 0;
     }
 }
--- src/test/java/ch/heigvd/labo/command/CleanTest.java
+++ src/test/java/ch/heigvd/labo/command/CleanTest.java
@@ -3,16 +3,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import ch.heigvd.labo.Main;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.io.TempDir;
 import picocli.CommandLine;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.apache.commons.io.FileUtils;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CleanTest {
-    static final String DIR_TO_REMOVE = "www/test_init";
+    static final String DIR_TO_REMOVE = "www/test_clean";
 
     /**
      * Create a test repertory
@@ -31,12 +34,11 @@ public class CleanTest {
 
     /**
      * Delete an existant repertory
-     */
+    */
     @Test
     @Order(1)
-    void shouldDeleteExistantRepertory()throws Exception{
-        File dir = new File(DIR_TO_REMOVE);
-        int exitCode = new CommandLine(new Clean()).execute(dir);
+    void shouldDeleteExistantRepertoryBuild()throws Exception{
+        int exitCode = new CommandLine(new Clean()).execute(DIR_TO_REMOVE);
         assertEquals(exitCode, 0);
         assertThrows(IOException.class, () -> {
             throw new IOException("Impossible de supprimer le répertoire");
@@ -44,17 +46,43 @@ public class CleanTest {
     }
 
     /**
-     * Try to delete an inexistant repertory
+     * Try to delete an inexistant repertory build
      */
     @Test
     @Order(2)
+    void shouldNotDeleteInexistantRepertoryBuild() throws Exception {
+        int exitCode = new CommandLine(new Clean()).execute(DIR_TO_REMOVE);
+        assertEquals(exitCode, 2);
+        assertThrows(IOException.class, () -> {
+            throw new IOException("Le répertoire est inexistant");
+        });
+    }
+
+    /**
+     * Try to delete an inexistant repertory
+    */
+    @Test
+    @Order(3)
     void shouldNotDeleteInexistantRepertory() throws Exception {
-        File dir = new File(DIR_TO_REMOVE);
-        int exitCode = new CommandLine(new Clean()).execute(dir);
+        int exitCode = new CommandLine(new Clean()).execute("www/inexistant");
         assertEquals(exitCode, 1);
         assertThrows(IOException.class, () -> {
             throw new IOException("Le répertoire est inexistant");
         });
+    }
+
+    @AfterAll
+    /**
+     * Une fois les tests terminés, le répertoire servant de test est supprimé
+     * afin de ne pas surcharger le projet
+     */
+    static void cleanRepertoryTest(){
+        File dir = new File("www/test_clean");
+        try {
+            FileUtils.deleteDirectory(dir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
