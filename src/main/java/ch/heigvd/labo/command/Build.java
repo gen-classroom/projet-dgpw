79a4297144deaea9a0bb7f47ec7edde54ae2a75d
Rotho1998 <rob.gaudin@hotmail.com>
2021-03-26 12:46:15 +0100

    Base de la commande build

--- src/main/java/ch/heigvd/labo/command/Build.java
+++ src/main/java/ch/heigvd/labo/command/Build.java
@@ -1,12 +1,37 @@
 package ch.heigvd.labo.command;
 
 import java.util.concurrent.Callable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(name = "build", description ="Build .... ???")
-public class Build implements Callable {
+@Command(name = "build", description ="Compile un site statique")
+public class Build implements Callable<Integer> {
+    static final String DIR_ROOT = "www/";
+    static final String DIR_TO_COMPILE = "/metadonnee";
+
+    @CommandLine.Option(names = "-d", description = "Répertoire du site statique")
+    static File siteDir;
+
     @Override public Integer call(){
-        System.out.printf("build");
-        return 1;
+        if(siteDir != null){
+            File dir = new File(DIR_ROOT + siteDir.getPath());
+            //Vérifie que le répertoire du site est existant
+            if (!dir.exists()) {
+                System.out.format("Impossible d'accéder au répertoire %s. Celui-ci est inexistant.\n",dir.getName());
+                return 1;
+            }
+
+            
+
+            return 0;
+        } else {
+            System.out.println("Le paramètre -d n'a pas été précisé !");
+            return 1;
+        }
     }
 }
