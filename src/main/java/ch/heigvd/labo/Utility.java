253891fd3dc6e8fac936228f2a47c3f82ea26492
Cassandre Wojciechowski <49147626+CassandreWoj@users.noreply.github.com>
2021-03-31 11:30:52 +0200

    Update Utility.java

--- src/main/java/ch/heigvd/labo/Utility.java
+++ src/main/java/ch/heigvd/labo/Utility.java
@@ -11,12 +11,12 @@ public final class Utility {
             "filesdir:  #inserer repertoire contenant les fichiers\n";
 
     public static final String MD_TEMPLATE = "<!--Template pour une page, à modifier comme vous" +
-            " le souhaiter ! (format Markdown) \n" +
+            " le souhaitez ! (format Markdown) \n" +
             "Syntaxe utile: \n *italique*\n **gras**\n tableau 2 lignes 2 colonnes:\n" +
             "|      |      |\n" +
             "| ---- | ---- |\n" +
             "|      |      | \n" +
-            "`code` \n bloc de code: \n ````html \n une image (ne pas oublier de copier l'image dans le répértoire" +
+            "`code` \n bloc de code: \n ````html \n une image (ne pas oublier de copier l'image dans le répertoire" +
             " ../metadonnee/image.png:\n ![Description](./image.png)-->\n\n" +
             "> titre:\n\n" + "> auteur:\n\n" + "> date:\n\n" + "-----\n" +
             "#TITRE 1\n" + "##TITRE 2\n" + "###TITRE 3\n" +
